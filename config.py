20a5b71b73603d2a2a06788d2097f7bc74228510
Alokchauhan110 <166166233+Alokchauhan110@users.noreply.github.com>
2024-04-20 22:17:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,72 +1,67 @@
+# Jishu Developer 
+# Don't Remove Credit 🥺
+# Telegram Channel @Madflix_Bots
+# Backup Channel @JishuBotz
+# Developer @JishuDeveloper
+
+
+
+
 import os
 import logging
 from logging.handlers import RotatingFileHandler
 
-#Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7128336686:AAHlDZpgvwtwdrwcuxwu0dSAbKn_Pjw2HkM")
 
-#Your API ID & API HASH from my.telegram.org [https://youtu.be/gZQJ-yTMkEo?si=H4NlUUgjsIc5btzH]
-#Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", "24979349"))
 
-#Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "ceef50502546a4ecdab59551496c4d57")
 
-#Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002102107986"))
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+APP_ID = int(os.environ.get("APP_ID", ""))
+API_HASH = os.environ.get("API_HASH", "")
 
-#OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", "6295345515"))
 
-#Port
-PORT = os.environ.get("PORT", "8080")
+OWNER = os.environ.get("OWNER", "") #Owner username
+OWNER_ID = int(os.environ.get("OWNER_ID", "")) #Owner user id
+DB_URL = os.environ.get("DB_URL", "")
+DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
 
-#Database 
-#Database [https://youtu.be/qFB0cFqiyOM?si=fVicsCcRSmpuja1A]
-DB_URI = "mongodb+srv://Rexonblack:Rexonblack123@rexonblack.knmbp2d.mongodb.net/?retryWrites=true&w=majority&appName=rexonblack"
-DB_NAME = os.environ.get("DATABASE_NAME", "rexonblack")
 
-#Shortner (token system) 
-# check my discription to help by using my refer link of shareus.io
-# 
-
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "api.upshrink.com")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "f2352b3fe7471646ed2bbf3801d931bab0362e21")
-VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
-IS_VERIFY = os.environ.get("IS_VERIFY", "True")
-TUT_VID = os.environ.get("TUT_VID","gojfsi/2")
-
-#force sub channel id, if you want enable force sub
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002073622834"))
 
+
+SECONDS = int(os.getenv("SECONDS", "600")) # auto delete in seconds
+
+
+
+PORT = os.environ.get("PORT", "8080")
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
-#start message
+
+
+
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
+
 try:
-    ADMINS=[]
-    for x in (os.environ.get("ADMINS", "6295345515").split()):
+    ADMINS=[7085541484]
+    for x in (os.environ.get("ADMINS", "5115691197 6273945163 6103092779 5231212075").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
-#Force sub message 
+
 FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
 
-#set your Custom Caption here, Keep None for Disable Custom Caption
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "This video/Photo/anything is available on the internet. its subsidiary channel doesn't produce any of them.")
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 
-#set True if you want to prevent users from forwarding files from bot
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
 
-#Set true if you want Disable your Channel Posts Share button
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
+USER_REPLY_TEXT = "<b>❌Don't send me messages to me if any problem then contact @rexonblood!</b>"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(6695586027)
+ADMINS.append(7085541484)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
@@ -88,3 +83,14 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
+   
+
+
+
+
+
+# Jishu Developer 
+# Don't Remove Credit 🥺
+# Telegram Channel @Madflix_Bots
+# Backup Channel @JishuBotz
+# Developer @JishuDeveloper
